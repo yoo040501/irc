1f1e8678101507abc7718485dd990c38918c25ff
Seungryeol Kim <seungryk@c5r3s5.42seoul.kr>
2025-01-02 15:59:31 +0900

    remove

--- obj/srcs/Channel.d
+++
@@ -1,2 +0,0 @@
-obj/srcs/Channel.o: srcs/Channel.cpp srcs/../includes/Channel.hpp \
-  srcs/../includes/Client.hpp
--- obj/srcs/command/User.d
+++
@@ -1,5 +0,0 @@
-obj/srcs/command/User.o: srcs/command/User.cpp \
-  srcs/command/../../includes/Server.hpp \
-  srcs/command/../../includes/Client.hpp \
-  srcs/command/../../includes/Message.hpp \
-  srcs/command/../../includes/Channel.hpp
